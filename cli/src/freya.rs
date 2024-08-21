d937ad8096aba9b73f6020ed4b80678116ff647f
Ewan Fox <ewan@ewanfox.com>
2024-08-21 08:31:41 -0400

    figured it out

--- cli/src/freya.rs
+++ cli/src/freya.rs
@@ -2,19 +2,29 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use freya::prelude::*;
-use freya::components::Button;
-use reqwest::Url;
-use std::sync::Arc;
 use crate::config::read_config;
 use crate::CliError;
-use crate::{ankiconnect::card_with_kanji, db::{new_cards, KanjiSrs}};
+use crate::{
+    ankiconnect::card_with_kanji,
+    db::{new_cards, KanjiSrs},
+};
+use freya::components::Button;
+use freya::prelude::*;
+use reqwest::Url;
+use std::sync::Arc;
 
-pub fn start_ui() {
-    launch_with_props(rapp, "Immerse", (700., 600.))
+use dioxus_router::prelude::{Outlet, Routable, Router};
+
+fn route() -> Element {
+    rsx!(Router::<Route> {})
 }
 
-fn rapp() -> Element {
+pub fn start_ui() {
+    launch_with_props(route, "Immerse", (700., 600.))
+}
+
+#[allow(non_snake_case)]
+fn Rapp() -> Element {
     let app = app();
     match app {
         Ok(el) => return el,
@@ -24,105 +34,124 @@ fn rapp() -> Element {
                 label {
                     "An error occurred!"
                 }
-            )
-        },
+            );
+        }
     }
 }
 
+#[derive(Routable, Clone, PartialEq)]
+#[rustfmt::skip]
+pub enum Route {
 
-fn app() -> Result<Element, CliError> {
+    #[route("/review")]
+    Rapp,
+    #[route("/")]
+    Home,
 
-    let mut current: Signal<String> = use_signal(|| String::new());
-    let mut kanji: Signal<char> = use_signal(|| ' ');
-    let n = new_cards();
-    if n.is_err() {
-        println!("{}", n.as_ref().unwrap_err());
-    }
-    let mut new: Signal<Vec<KanjiSrs>> = use_signal(|| n.unwrap_or_else(|_| vec!()));
+}
 
-    let conf = Arc::new(read_config()?);
-
-
-    
-    
-    Ok(rsx!(
+#[allow(non_snake_case)]
+fn Home() -> Element {
+    rsx!(
         rect {
             height: "50%",
             width: "100%",
             main_align: "center",
             cross_align: "center",
-            background: "rgb(0, 119, 182)",
-            color: "white",
-            shadow: "0 4 20 5 rgb(0, 0, 0, 80)",
-            rect {
-                main_align: "center",
-                cross_align: "center",
-                label {
-                    font_size: "25",
-                    "{kanji}"
-                }
-                label {
-                    font_size: "75",
-                    font_weight: "bold",
-                    "{current}"
-                }
-            }
-        }
-        /*rect {
-            height: "fill",
-            direction: "vertical",
-            width: "100%",
-            main_align: "center"
-        }*/
-        rect {
-            direction: "horizontal",
-            width: "100%",
-            height: "fill",
-            main_align: "center",
-            cross_align: "center",
             Button {
-                onclick: move |_| {
-                    if let Some(kanji_vec) = new.pop() {
-                        let conf_clone = Arc::clone(&conf);
-                        spawn(async move {
-                            let e = card_with_kanji(kanji_vec.kanji, &conf_clone).await;
-                            kanji.set(kanji_vec.kanji);
-                            match e {
-                                Ok(result) => {
-                                    *current.write() = result.result[0].fields.get("Word").unwrap().value.clone();
-                                },
-                                Err(ref err) => {eprintln!("{}", err);},
-                            }
-                        });
+                Link {
+                    to: Route::Rapp,
+                    label {
+                        "Review!"
                     }
-                },
-                label {
-                    "Next"
                 }
             }
-            /*Button {
-                label {
-                    "Again"
-                }
-            }
-            Button {
-                label {
-                    "Hard"
-                }
-            }
-            Button {
-                label {
-                    "Good"
-                },
-                onclick: |_| {
-                    println!("Good");
-                }
-            }
-            Button {
-                label {
-                    "Easy"
-                }
-            }*/
         }
+    )
+}
+
+struct Question {
+    word: String,
+}
+
+fn app() -> Result<Element, CliError> {
+    let mut current: Signal<Option<KanjiSrs>> = use_signal(|| None);
+    let n = new_cards();
+    if n.is_err() {
+        println!("{}", n.as_ref().unwrap_err());
+    }
+    let mut  new: Signal<Vec<KanjiSrs>> = use_signal(|| n.unwrap_or_else(|_| vec![]));
+
+    let conf = Arc::new(read_config()?);
+
+    let q = use_resource(move || {
+        let conf_clone = Arc::clone(&conf);
+        return async move {
+            if let Some(k) = current.read().as_ref() {
+
+
+                let e = card_with_kanji(k.kanji, &conf_clone).await;
+                match e {
+                    Ok(result) => {
+                        return Ok(Question {
+                            word: result.result[0].fields.get("Word").unwrap().value.clone(),
+                        })
+                    }
+                    Err(err) => return Err(err),
+                }
+            } else {
+                return Err(CliError::Custom("Something is messed up".to_string()));
+            }
+        };
+    });
+
+    use_effect(move || {current.set(new.pop());});
+
+
+    Ok(rsx!(
+
+        match &*q.read() {
+            Some(Ok(res)) => {
+                rsx!{
+                    rect {
+                        height: "50%",
+                        width: "100%",
+                        main_align: "center",
+                        cross_align: "center",
+                        background: "rgb(0, 119, 182)",
+                        color: "white",
+                        shadow: "0 4 20 5 rgb(0, 0, 0, 80)",
+                        rect {
+                            main_align: "center",
+                            cross_align: "center",
+                            label {
+                                font_size: "25",
+            
+                            }
+                            label {
+                                font_size: "75",
+                                font_weight: "bold",
+                                "{res.word}"
+                            }
+                        }
+                    }
+                }
+            },
+            Some(Err(err)) => {
+                rsx!{
+                    label {
+                        "{err}"
+                    }
+                }
+            },
+            None => {
+                rsx!{
+                    label {
+                        "Loading..."
+                    }
+                }
+            }
+        }
+        
     ))
 }
